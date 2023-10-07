26feeea107edbe2c082f9a2106efd667c38153f6
AB-programming <WFB15376253289@outlook.com>
2023-10-07 18:42:44 +0800

    Update: site page

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,9 +1,10 @@
 module.exports = {
     title: '济南KUG',
-    description: '这里是北京 Kotlin 用户组，欢迎所有对 Kotlin 感兴趣的开发者加入用户组',
+    description: '这里是济南 Kotlin 用户组，欢迎所有对 Kotlin 感兴趣的开发者加入用户组',
     lang: 'zh-CN',
     head: [['link', { rel: 'icon', type: 'image/svg+xml', href: 'kug.png' }]],
     themeConfig: {
+        logo: { src: '/public/kug.png', width: 24, height: 24 },
         repo: 'jinan-kug/.github',
         repoLabel: 'GitHub',
         docsDir: 'docs',
@@ -16,29 +17,34 @@ module.exports = {
             { text: 'Kotlin中文', link: 'https://www.kotlincn.net/' },
             { text: 'GitHub社区', link: 'https://github.com/jinan-kug/'}
         ],
+        socialLinks: [
+            { icon: 'github', link: 'https://github.com/jinan-kug/jinan-kug.github.io' }
+        ],
         sidebar: {
-            '/': [
-                {
-                    text: '首页',
-                    link: '/'
-                },
+            '/resources': [
                 {
                     text: '简介',
-                    link: '/intro/index.md'
+                    link: '/resources/intro/index.md'
                 },
                 {
                     text: '快速开始',
-                    link: '/start/index.md'
+                    link: '/resources/start/index.md'
                 },
                 {
                     text: '协程',
-                    link: '/coroutine/index.md'
+                    link: '/resources/coroutine/index.md'
                 },
                 {
                     text: '关于我们',
-                    link: '/about/index.md'
+                    link: 'resources/about/index.md'
                 }
             ]
+        },
+        footer: {
+            copyright: 'Copyright © 2023 jinan-kug Powered by VitePress'
+        },
+        search: {
+            provider: 'local'
         }
     }
 }
---
+++ docs/join/index.md
@@ -0,0 +1,18 @@
+# 可通过以下方式加入济南KUG
+
+## 微信群
+<img src="/public/group.png" width="300">
+
+## 负责人微信
+若上方微信群失效,可加负责人微信，拉你入群
+
+负责人微信号：**ABprogramming**
+
+<img src="/public/me.jpeg" width="300">
+
+## 负责人邮箱
+可以通过邮箱联系我
+<a href="mailto:abprogramming007@gmail.com">abprogramming007@gmail.com</a>
+
+## 查看国内其他KUG
+<a href="https://mp.weixin.qq.com/s/rZEuSU2Nj3S9XGzpkFr-Jw" target="_blank">国内 Kotlin User Group 信息整理（23年7月）</a>
